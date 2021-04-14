30b586f882c7b3153eabf81c9d2a25bc277e4928
Offender <81564919+O-ffender@users.noreply.github.com>
2021-04-14 21:24:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ import colorama
 from colorama import Fore
  
 token = "YOUR_TOKEN" # Paste your discord token here!
+prefix = "$" # Customize the prefix.
+title = '''ran by Offender (:''' # Input a title here, like, Ran By Offender!
+linky = "https://put.a.random.link.here" # Over here you make a fake link, like https://offender.on.top or something anything
+footer = "Put a footer here lol" # This will be at the end of the image, Example: All Hail Me!
  
-prefix = "+" # Customize the prefix.
- 
-title = '''ran by misspoken (:''' # Input a title here, like, Ran By Misspoken!
- 
-linky = "https://put.a.random.link.here" # Over here you make a fake link, like https://misspoken.on.top or something anything
- 
-footer = "Put a footer here lol" # This will be at the end of the image, Example: Misspoken owns you!
- 
-# Example of command, +embed https://cdn.discordapp.com/attachments/811716182481698847/811729488861724722/Random-FB-BANNER.png
+# Example of command, $embed https://cdn.discordapp.com/attachments/811716182481698847/811729488861724722/Random-FB-BANNER.png
  
 client = commands.Bot(command_prefix=prefix, case_insensitive=True, self_bot=True)
  
@@ -26,7 +22,7 @@ os.system('cls' if os.name == 'nt' else 'clear')
 async def onready():
     print(f'''{Fore.RED}
  
-Pays.Host Alt Self-Bot! Made by Misspoken! (:
+Alternative Discord Image Hosting Self Bot! (:
  
 __{prefix}embed <link>
     ''' + Fore.RESET)
